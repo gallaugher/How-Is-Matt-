209409d589acd29a7967958c1566dfe670ca7849
Gallaugher <john.gallaugher@bc.edu>
2017-09-19 19:09:29 -0400

    Completed "How is Matt?" Exercise

--- How Is Matt?/ViewController.swift
+++ How Is Matt?/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alrightLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func alrightStepper(_ sender: UIStepper) {
+        alrightLabel.text = String(Int(stepper.value))
     }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer, loops: Int) {
+        // Can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName) {
+            // check if sound.data is a sound file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.numberOfLoops = loops - 1
+                audioPlayer.play()
+            } catch {
+                // if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            // if reading in the NSDataAsset didn't work, report the error.
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    @IBAction func tellMePressed(_ sender: UIButton) {
+        let loops = Int(alrightLabel.text!)!
+        if loops > 0 {
+            playSound(soundName: "alright", audioPlayer: &audioPlayer, loops: loops)
+        }
 
+    }
+    
 
 }
 
